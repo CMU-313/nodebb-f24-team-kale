4d442494c8f55e39665c4c5f0946009a712f1f10
Ariel <ychiu3@andrew.cmu.edu>
2024-10-24 12:46:36 -0400

    fix lint

--- analyzeIroh.js
+++ analyzeIroh.js
@@ -1,15 +1,17 @@
+'use strict';
+
 const fs = require('fs');
 const Iroh = require('iroh');
 
-let code = fs.readFileSync('src/prestart.js', 'utf8');
+const code = fs.readFileSync('src/prestart.js', 'utf8');
 
-let stage = new Iroh.Stage(code);
+const stage = new Iroh.Stage(code);
 
-let varListener = stage.addListener(Iroh.VAR);
+const varListener = stage.addListener(Iroh.VAR);
 
-varListener.on("after", (e) => {
-  const output = `Variable assigned: ${e.name} = ${e.value}\n`;
-  fs.appendFileSync('iroh-output.txt', output);
+varListener.on('after', (e) => {
+    const output = `Variable assigned: ${e.name} = ${e.value}\n`;
+    fs.appendFileSync('iroh-output.txt', output);
 });
 
 eval(stage.script);
