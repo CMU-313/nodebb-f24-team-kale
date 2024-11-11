097f4677d57c614e1c46c031557a8a9a9b5b24b3
Kayla Lei <kaylalei@macbook-pro-538.wifi.local.cmu.edu>
2024-11-11 18:11:43 -0500

    fix lint errors

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,11 +1,10 @@
-var request = require('request');
+'use strict';
 
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    // Edit the translator URL below
-    const TRANSLATOR_API = "https://translator-service-kale.azurewebsites.net/"
-    const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
-    const data = await response.json();
-    return [data["is_english"], data["translated_content"]]
-}
+	const TRANSLATOR_API = 'https://translator-service-kale.azurewebsites.net/';
+	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
+	const data = await response.json();
+	return [data.isEnglish, data.translatedContent];
+};
