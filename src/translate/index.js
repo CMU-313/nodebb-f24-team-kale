cacb97d067698392841f9ecf556be86e8fb15686
Kayla Lei <kaylalei@macbook-pro-538.wifi.local.cmu.edu>
2024-11-11 18:38:40 -0500

    updated translate api

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,10 +1,11 @@
-'use strict';
+var request = require('request');
 
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-	const TRANSLATOR_API = 'https://translator-service-kale.azurewebsites.net/';
-	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
-	const data = await response.json();
-	return [data.isEnglish, data.translatedContent];
-};
+    // Edit the translator URL below
+    const TRANSLATOR_API = "https://nodebb-f24-translator.azurewebsites.net/"
+    const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
+    const data = await response.json();
+    return [data["is_english"], data["translated_content"]]
+}
